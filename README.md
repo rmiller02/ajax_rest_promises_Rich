d16bd30278f9df53ebf7ccf4b5ac17524e2a67b7
rmiller02 <r_miller02@yahoo.com>
2020-04-15 16:00:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ajax_rest_promises_Rich
---
+++ ajaxrestpromises.js
@@ -0,0 +1,11 @@
+
+1.
+
+document.getElementById('getMessage').onclick = function(){};
+
+
+2.
+
+document.getElementsByClassName('message')[0].textContent="Here is the message";
+
+
